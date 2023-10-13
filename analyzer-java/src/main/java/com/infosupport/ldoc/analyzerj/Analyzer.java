3b95448f616b73be1f58a8a6585b7f3f0a01ced0
Wander Nauta <info@wandernauta.nl>
2023-10-13 12:17:08 +0200

    Add basic Maven plugin and usage example
    The plugin can be bound to the process-sources Maven phase in a project and will then generate a livingdocumentation.json file in the target directory.
    
    Any JARs that are part of the compilation classpath are added to the parser to resolve type information.
    
    The current plugin is not really configurable; it always does the above thing.

--- analyzer-java/src/main/java/com/infosupport/ldoc/analyzerj/Analyzer.java
+++ analyzer-java/src/main/java/com/infosupport/ldoc/analyzerj/Analyzer.java
@@ -3,10 +3,14 @@ package com.infosupport.ldoc.analyzerj;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.resolution.SymbolResolver;
+import com.github.javaparser.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.TypeSolverBuilder;
 import com.github.javaparser.symbolsolver.utils.SymbolSolverCollectionStrategy;
-import com.github.javaparser.utils.ProjectRoot;
+import com.github.javaparser.utils.CollectionStrategy;
 import com.github.javaparser.utils.SourceRoot;
 import com.infosupport.ldoc.analyzerj.descriptions.Description;
 import java.io.IOException;
@@ -21,11 +25,25 @@ public class Analyzer {
     this.objectMapper = objectMapper;
   }
 
+  private TypeSolver typeSolverFor(AnalysisJob job) throws IOException {
+    var typeSolverBuilder = new TypeSolverBuilder().withCurrentJRE().withSourceCode(job.project());
+
+    for (String cp : job.classpath()) {
+      if (cp.toLowerCase().endsWith(".jar")) {
+        typeSolverBuilder = typeSolverBuilder.withJAR(cp);
+      }
+    }
+
+    return typeSolverBuilder.build();
+  }
+
   public void analyze(AnalysisJob job) throws IOException {
-    ProjectRoot projectRoot = new SymbolSolverCollectionStrategy().collect(job.project());
+    ParserConfiguration parserConfiguration = new ParserConfiguration()
+        .setSymbolResolver(new JavaSymbolSolver(typeSolverFor(job)));
+    CollectionStrategy strategy = new SymbolSolverCollectionStrategy(parserConfiguration);
     List<Description> descriptions = new ArrayList<>();
 
-    for (SourceRoot sourceRoot : projectRoot.getSourceRoots()) {
+    for (SourceRoot sourceRoot : strategy.collect(job.project()).getSourceRoots()) {
       List<ParseResult<CompilationUnit>> results = sourceRoot.tryToParse();
       SymbolResolver solver = sourceRoot.getParserConfiguration().getSymbolResolver().orElseThrow();
 
