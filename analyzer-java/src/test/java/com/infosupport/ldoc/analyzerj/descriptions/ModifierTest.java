1f233973b0727a49361d214140428a50c1b8dc24
chaka <chakaroun_ali@yahoo.com>
2023-10-25 21:43:41 +0200

    Extended ModifierTest to cover the other modifier types.

--- analyzer-java/src/test/java/com/infosupport/ldoc/analyzerj/descriptions/ModifierTest.java
+++ analyzer-java/src/test/java/com/infosupport/ldoc/analyzerj/descriptions/ModifierTest.java
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.Test;
 class ModifierTest {
 
   @Test
+  void modifier_with_public_equivalent_returns_equivalent() {
+
+  }
+  @Test
   void modifier_with_no_equivalent_returns_none() {
     assertEquals(
         Modifier.NONE,
@@ -20,8 +24,23 @@ class ModifierTest {
   @Test
   void modifier_with_direct_equivalent_returns_equivalent() {
     assertEquals(
+        Modifier.PUBLIC,
+        Modifier.valueOf(com.github.javaparser.ast.Modifier.publicModifier()));
+    assertEquals(
         Modifier.PRIVATE,
         Modifier.valueOf(com.github.javaparser.ast.Modifier.privateModifier()));
+    assertEquals(
+        Modifier.PROTECTED,
+        Modifier.valueOf(com.github.javaparser.ast.Modifier.protectedModifier()));
+    assertEquals(
+        Modifier.ABSTRACT,
+        Modifier.valueOf(com.github.javaparser.ast.Modifier.abstractModifier()));
+    assertEquals(
+        Modifier.STATIC,
+        Modifier.valueOf(com.github.javaparser.ast.Modifier.staticModifier()));
+    assertEquals(
+        Modifier.EXTERN,
+        Modifier.valueOf(com.github.javaparser.ast.Modifier.nativeModifier()));
   }
 
   @Test
