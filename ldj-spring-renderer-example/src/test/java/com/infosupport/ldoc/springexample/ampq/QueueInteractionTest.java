62b6a5bd4a5374ecfd9e13d84138eacf20517bed
Guus Grievink <gs.grievink@gmail.com>
2023-11-27 16:04:53 +0100

    Now adhering to style guides for the tests

--- ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/ampq/QueueInteractionTest.java
+++ ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/ampq/QueueInteractionTest.java
@@ -1,6 +1,8 @@
 package com.infosupport.ldoc.springexample.ampq;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,16 +31,22 @@ class QueueInteractionTest {
   @Test
   void filter_empty_post_list() {
     List<QueueInteraction> interactions = new ArrayList<>();
-    assertIterableEquals(new ArrayList<QueueInteraction>(), QueueInteraction.getAllPosts(interactions));
+    assertIterableEquals(new ArrayList<QueueInteraction>(),
+        QueueInteraction.getAllPosts(interactions));
   }
 
   @Test
   void filterReadsReactingToThis() {
-    QueueInteraction post = new QueueInteraction("Kees", QueueInteractionKind.POST, "q1", "msg1", null);
-    QueueInteraction read = new QueueInteraction("Henk", QueueInteractionKind.READ, "q1", "msg1", null);
-    QueueInteraction read2 = new QueueInteraction("Henks alterego", QueueInteractionKind.READ, "q1", "msg1", null);
-    QueueInteraction readDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.READ, "q1", "otherMsg", null);
-    QueueInteraction readDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.READ, "otherQueue", "msg1", null);
+    QueueInteraction post = new QueueInteraction("Kees", QueueInteractionKind.POST, "q1", "msg1",
+        null);
+    QueueInteraction read = new QueueInteraction("Henk", QueueInteractionKind.READ, "q1", "msg1",
+        null);
+    QueueInteraction read2 = new QueueInteraction("Henks alterego", QueueInteractionKind.READ, "q1",
+        "msg1", null);
+    QueueInteraction readDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.READ, "q1",
+        "otherMsg", null);
+    QueueInteraction readDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.READ,
+        "otherQueue", "msg1", null);
     List<QueueInteraction> readInteractions = Arrays.asList(readDecoy1, read, readDecoy2, read2);
 
     List<QueueInteraction> expected = Arrays.asList(read, read2);
@@ -48,14 +56,19 @@ class QueueInteractionTest {
 
   @Test
   void is_read_on_post() {
-    QueueInteraction read = new QueueInteraction("henk", QueueInteractionKind.READ, "q1", "msg1", null);
-    QueueInteraction post = new QueueInteraction("kees", QueueInteractionKind.POST, "q1", "msg1", null);
-    QueueInteraction postDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.POST, "q1", "otherMsg", null);
-    QueueInteraction postDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.POST, "otherQueue", "msg1", null);
-
+    QueueInteraction read = new QueueInteraction("henk", QueueInteractionKind.READ, "q1", "msg1",
+        null);
+    QueueInteraction post = new QueueInteraction("kees", QueueInteractionKind.POST, "q1", "msg1",
+        null);
     assertTrue(read.isReadOnPost(post));
     assertFalse(post.isReadOnPost(read));
+
+    QueueInteraction postDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.POST, "q1",
+        "otherMsg", null);
     assertFalse(read.isReadOnPost(postDecoy1));
+
+    QueueInteraction postDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.POST,
+        "otherQueue", "msg1", null);
     assertFalse(read.isReadOnPost(postDecoy2));
   }
 }
