7b5e97e3d9bbb2d888b1f830c58f87cd548350c9
Guus Grievink <gs.grievink@gmail.com>
2023-12-04 14:35:39 +0100

    rename from ampq to amqp

--- ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionTest.java
+++ ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionTest.java
@@ -1,4 +1,4 @@
-package com.infosupport.ldoc.springexample.ampq;
+package com.infosupport.ldoc.springexample.amqp;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
