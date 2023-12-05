56680bcdbe7c6752db4a2dbef16bd322b38313f9
Guus Grievink <gs.grievink@gmail.com>
2023-12-05 15:06:48 +0100

    Added JavaDocs for new public methods in the Spring AMQP example.

--- ldj-spring-amqp-example/src/main/java/org/example/domain/Receipt.java
+++ ldj-spring-amqp-example/src/main/java/org/example/domain/Receipt.java
@@ -8,11 +8,17 @@ public class Receipt implements Serializable {
 
   public final double price;
 
+  /**
+   * Creates a receipt from a SandwichOrder. Always sets the price to the fixed value of 8.75.
+   */
   public Receipt(SandwichOrder sandwichOrder) {
     this.sandwichOrder = sandwichOrder;
     this.price = 8.75; // All sandwiches are created equal
   }
 
+  /**
+   * Represents a receipt as a String. Fully displaying the order along with the price.
+   */
   @Override
   public String toString() {
     return "Receipt for " + this.sandwichOrder + " with price = " + this.price;
