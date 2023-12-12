2f8f8324e2ccc525294638ffdf82c954a009028f
Wander Nauta <info@wandernauta.nl>
2023-12-12 20:46:20 +0100

    Add scaffolding for project reader library

---
+++ ldj-renderer-support/src/main/java/com/infosupport/ldoc/support/Node.java
@@ -0,0 +1,6 @@
+package com.infosupport.ldoc.support;
+
+public interface Node {
+  void accept(Visitor v);
+
+}
---
+++ ldj-renderer-support/src/main/java/com/infosupport/ldoc/support/Struct.java
@@ -0,0 +1,5 @@
+package com.infosupport.ldoc.support;
+
+public interface Struct extends Type {
+
+}
