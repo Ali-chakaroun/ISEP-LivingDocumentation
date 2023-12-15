b743a64948eb92cb07637024ac122313b1817be5
Wander Nauta <info@wandernauta.nl>
2023-12-15 18:59:50 +0100

    Add stubs to MethodImpl

--- ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/Method.java
+++ ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/Method.java
@@ -1,8 +1,18 @@
 package com.infosupport.ldoc.reader;
 
+import java.util.stream.Stream;
+
 public interface Method extends Node {
 
   String name();
+
+  String returnType();
+
+  Stream<Parameter> parameters();
+
+  Stream<Statement> statements();
+
+  Stream<Attribute> attributes();
 
   DocumentationComment documentationComment();
 
