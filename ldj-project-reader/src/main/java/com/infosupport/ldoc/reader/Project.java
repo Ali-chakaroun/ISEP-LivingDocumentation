2a7646f78a29830f69aa10bab9df9db51ee8d768
Wander Nauta <info@wandernauta.nl>
2023-12-15 22:09:14 +0100

    Fix non-Javadoc Checkstyle issues, re-enable non-Javadoc Checkstyle check

--- ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/Project.java
+++ ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/Project.java
@@ -3,10 +3,14 @@ package com.infosupport.ldoc.reader;
 import java.util.stream.Stream;
 
 public interface Project extends Node {
+
   Stream<Type> allTypes();
 
   Stream<Class> classes();
+
   Stream<Interface> interfaces();
+
   Stream<Enum> enums();
+
   Stream<Struct> structs();
 }
