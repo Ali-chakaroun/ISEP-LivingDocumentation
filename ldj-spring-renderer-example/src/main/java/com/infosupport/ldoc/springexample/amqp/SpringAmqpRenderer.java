b93b84676acd8ff0f7c43274ec5f844286d12a13
Guus Grievink <gs.grievink@gmail.com>
2023-12-16 14:34:13 +0100

    Rename all occurences of read/post to the Spring AMQP receive/send terminology

--- ldj-spring-renderer-example/src/main/java/com/infosupport/ldoc/springexample/amqp/SpringAmqpRenderer.java
+++ ldj-spring-renderer-example/src/main/java/com/infosupport/ldoc/springexample/amqp/SpringAmqpRenderer.java
@@ -27,11 +27,12 @@ public class SpringAmqpRenderer {
       + "annotation.RabbitHandler";
 
   /**
+   * Jackson JsonNode helper function.
    * Check if contains attribute with specified type and return that attribute node.
    *
    * @return The attribute JsonNode with attributeType, null if does not exist
    */
-  public static JsonNode containsAttribute(JsonNode node, String attributeType) {
+  public static JsonNode getAttributeWithType(JsonNode node, String attributeType) {
     for (JsonNode att : node.path("Attributes")) {
       if (att.path("Type").asText("").equals(attributeType)) {
         return att;
@@ -41,70 +42,9 @@ public class SpringAmqpRenderer {
   }
 
   /**
-   * Whether a node is a method node that handles the reading of a queue message.
-   * Pragmatically checks whether a presumed method node contains a @RabbitHandler annotation
-   */
-  public static boolean isQueueReadMethod(JsonNode methodNode) {
-    return containsAttribute(methodNode, RABBIT_HANDLER_ANNOTATION) != null;
-  }
-
-  /**
-   * Tries to extract a post queue interaction. This can either be from: 1. a convertAndSend(queue,
-   * msg) method invocation 2. Rabbit listener with the @SendTo annotation.
+   * Jackson JsonNode helper function. Presumes node is array type.
    *
-   * @param node      JsonNode to check
-   * @param className class name that is attached to the interaction IF  such interaction is found
-   * @return Extracted QueueInteraction from node. Null if not found
-   */
-  public static QueueInteraction extractQueuePost(JsonNode node, String className) {
-    // convertAndSend statement type
-    if (node.path("$type").asText("").contains("InvocationDescription")
-        && node.path("Name").asText("").equals("convertAndSend")
-        && node.path("ContainingType").asText("").equals(RABBIT_TEMPLATE_CLASS)) {
-      String queue = node.path("Arguments").path(0).path("Text").asText();
-      String msgType = stripName(
-          node.path("Arguments").path(1).path("Type").asText()); // Full qualified name
-      return new QueueInteraction(className, QueueInteractionKind.POST, queue, msgType,
-          null); // Convert to basic name to allow cross application queue communications
-    }
-
-    // Listen method with return type
-    JsonNode sendToAttribute = containsAttribute(node, RABBIT_SEND_TO_ANNOTATION);
-    if (isQueueReadMethod(node) && sendToAttribute != null) {
-      // So node is both a method and contains the SendTo annotation
-      String queue = sendToAttribute.path("Arguments").path(0).path("Value").asText();
-      String msgType = stripName(node.path("ReturnType")
-          .asText()); // Convert to basic name to allow cross application queue communications
-      return new QueueInteraction(className, QueueInteractionKind.POST, queue, msgType, null);
-    }
-    return null;
-  }
-
-  /**
-   * Recursive method to retrieve all posts to a queue from a node and all its children.
-   *
-   * @param node      Node to check
-   * @param className class name that is attached to interactions that are found
-   * @return A list of Post Queue interactions that were found
-   */
-  public static List<QueueInteraction> findQueuePosts(JsonNode node, String className) {
-    List<QueueInteraction> interactions = new ArrayList<>(0);
-
-    QueueInteraction found = extractQueuePost(node, className);
-    if (found != null) {
-      interactions.add(found);
-    }
-
-    for (JsonNode child : node) {
-      interactions.addAll(findQueuePosts(child, className));
-    }
-    return interactions;
-  }
-
-  /**
-   * Presumes node is array type.
-   *
-   * @param nodes JsonNode (presumed to be of Json Array type
+   * @param nodes JsonNode (presumed to be of Json Array type)
    * @param key   the requested key of the JsonNode
    * @param value the requested value of the JsonNode
    * @return Json node (object) which contains value associated to key
@@ -123,12 +63,74 @@ public class SpringAmqpRenderer {
   }
 
   /**
+   * Whether a node is a method node that handles the receiving of a queue message.
+   * Pragmatically checks whether a presumed method node contains a @RabbitHandler annotation
+   */
+  public static boolean isQueueReceiveMethod(JsonNode methodNode) {
+    return getAttributeWithType(methodNode, RABBIT_HANDLER_ANNOTATION) != null;
+  }
+
+  /**
+   * Tries to extract a send queue interaction. This can either be from: 1. a convertAndSend(queue,
+   * msg) method invocation 2. Rabbit listener with the @SendTo annotation.
+   *
+   * @param node      JsonNode to check
+   * @param className class name that is attached to the interaction IF  such interaction is found
+   * @return Extracted QueueInteraction from node. Null if not found
+   */
+  public static QueueInteraction extractQueueSend(JsonNode node, String className) {
+    // convertAndSend statement type
+    if (node.path("$type").asText("").contains("InvocationDescription")
+        && node.path("Name").asText("").equals("convertAndSend")
+        && node.path("ContainingType").asText("").equals(RABBIT_TEMPLATE_CLASS)) {
+      // Extract the queue from the convertAndSend statement
+      String queue = node.path("Arguments").path(0).path("Text").asText();
+      String msgType = stripName(
+          node.path("Arguments").path(1).path("Type").asText()); // Full qualified name
+      return new QueueInteraction(className, QueueInteractionKind.SEND, queue, msgType,
+          null); // Convert to basic name to allow cross application queue communications
+    }
+
+    // Listen method with return type
+    JsonNode sendToAttribute = getAttributeWithType(node, RABBIT_SEND_TO_ANNOTATION);
+    if (isQueueReceiveMethod(node) && sendToAttribute != null) {
+      // So node is both a method and contains the SendTo annotation
+      String queue = sendToAttribute.path("Arguments").path(0).path("Value").asText();
+      String msgType = stripName(node.path("ReturnType")
+          .asText()); // Convert to basic name to allow cross application queue communications
+      return new QueueInteraction(className, QueueInteractionKind.SEND, queue, msgType, null);
+    }
+    return null;
+  }
+
+  /**
+   * Recursive method to retrieve all sends to a queue from a node and all its children.
+   *
+   * @param node      Node to check
+   * @param className class name that is attached to interactions that are found
+   * @return A list of Send Queue interactions that were found
+   */
+  public static List<QueueInteraction> findQueueSends(JsonNode node, String className) {
+    List<QueueInteraction> interactions = new ArrayList<>(0);
+
+    QueueInteraction found = extractQueueSend(node, className);
+    if (found != null) {
+      interactions.add(found);
+    }
+
+    for (JsonNode child : node) {
+      interactions.addAll(findQueueSends(child, className));
+    }
+    return interactions;
+  }
+
+  /**
    * From a JsonNode, gather all QueueInteractions.
    *
    * @param root root Node of the Json tree corresponding to a LivingDocumentation analysis Json
    *             file
-   * @return a list of all top-level interactions (note that post interactions that happen in a read
-   *             interaction are nested)
+   * @return a list of all top-level interactions (note that send interactions that happen in a
+   *             receive-interaction are nested)
    */
   public static List<QueueInteraction> findAllQueueInteractions(JsonNode root) {
     List<QueueInteraction> interactions = new ArrayList<>();
@@ -137,7 +139,7 @@ public class SpringAmqpRenderer {
       String className = typeDesc.path("FullName").textValue();
 
       //check if class is listener
-      JsonNode rabbitListenerAnnotation = containsAttribute(typeDesc, RABBIT_LISTENER_ANNOTATION);
+      JsonNode rabbitListenerAnnotation = getAttributeWithType(typeDesc, RABBIT_LISTENER_ANNOTATION);
       if (rabbitListenerAnnotation != null) { // I.e., this class is a RabbitListener
         // Note that the queue is the same for all methods within a RabbitListener
         JsonNode queueArgument = getNodeWithKeyAndValue(rabbitListenerAnnotation.path("Arguments"),
@@ -146,27 +148,27 @@ public class SpringAmqpRenderer {
             != null; // TODO This should not happen, how nicely do we want to handle this?
         String queue = queueArgument.path("Value").asText();
 
-        //Check for listen methods
+        //Check for receive/listen methods
         for (JsonNode method : typeDesc.path("Methods")) {
-          if (isQueueReadMethod(method)) {
+          if (isQueueReceiveMethod(method)) {
             // Note the type is stripped to support messages across different applications
             String messageType = stripName(
                 method.path("Parameters").path(0).path("Type").textValue());
-            // Create and add read interaction
-            QueueInteraction newRead = new QueueInteraction(className, QueueInteractionKind.READ,
+            // Create and add receive interaction
+            QueueInteraction newReceive = new QueueInteraction(className, QueueInteractionKind.RECEIVE,
                 queue, messageType, new ArrayList<>(0));
-            // Add all posts that happen within this read
-            newRead.reactionToRead().addAll(findQueuePosts(method, className));
+            // Add all sends that happen within this receive
+            newReceive.reactionToReceive().addAll(findQueueSends(method, className));
 
-            interactions.add(newRead);
+            interactions.add(newReceive);
           } else {
-            // Check for non-listening methods whether posts are made
-            interactions.addAll(findQueuePosts(typeDesc, className));
+            // Check for non-listening methods whether messages are sent to a queue
+            interactions.addAll(findQueueSends(typeDesc, className));
           }
         }
       } else {
-        // Check for non-listener classes
-        interactions.addAll(findQueuePosts(typeDesc, className));
+        // Check for non-receiving/listener classes
+        interactions.addAll(findQueueSends(typeDesc, className));
       }
 
     }
@@ -180,24 +182,24 @@ public class SpringAmqpRenderer {
    * @param out                             writer where PlantUML is written to
    * @param allInteractions                 all interactions associated with the source
    *                                        applications
-   * @param relevantPostInteractionsToReply list of Post Queue interactions that should be rendered
+   * @param relevantSendInteractionsToReply list of Send Queue interactions that should be rendered
    *                                        for this cycle
    */
   static void renderInteractions(PrintWriter out, List<QueueInteraction> allInteractions,
-      List<QueueInteraction> relevantPostInteractionsToReply) {
-    // Loop over all top-level post interactions
-    for (QueueInteraction postInteraction : relevantPostInteractionsToReply) {
-      // Loop over all read reactions that get triggered after the postInteraction
-      for (QueueInteraction readInteraction : postInteraction.filterReadsReactingToThis(
+      List<QueueInteraction> relevantSendInteractionsToReply) {
+    // Loop over all top-level send interactions
+    for (QueueInteraction sendInteraction : relevantSendInteractionsToReply) {
+      // Loop over all read reactions that get triggered after the sendInteraction
+      for (QueueInteraction receiveInteraction : sendInteraction.filterReceivesReactingToThis(
           allInteractions)) {
-        out.printf("%s -[#ForestGreen]> %s : %s\n", postInteraction.actor(),
-            readInteraction.actor(),
-            stripName(readInteraction.messageType()));
+        out.printf("%s -[#ForestGreen]> %s : %s\n", sendInteraction.actor(),
+            receiveInteraction.actor(),
+            stripName(receiveInteraction.messageType()));
 
-        out.printf("activate %s\n", readInteraction.actor());
-        // render all post queue interactions that are instantiated from this read interaction
-        renderInteractions(out, allInteractions, readInteraction.reactionToRead());
-        out.printf("deactivate %s\n", readInteraction.actor());
+        out.printf("activate %s\n", receiveInteraction.actor());
+        // render all send queue interactions that are instantiated from this receive interaction
+        renderInteractions(out, allInteractions, receiveInteraction.reactionToReceive());
+        out.printf("deactivate %s\n", receiveInteraction.actor());
       }
     }
   }
@@ -219,10 +221,10 @@ public class SpringAmqpRenderer {
           participant);
     }
 
-    // Note: requires there to be at least one queue post outside a queue read context
-    List<QueueInteraction> onlyPostInteractions = interactions.stream()
-        .filter(i -> i.kind() == QueueInteractionKind.POST).toList();
-    renderInteractions(bufferWriter, interactions, onlyPostInteractions);
+    // Note: requires there to be at least one queue send outside a queue read context
+    List<QueueInteraction> onlySendInteractions = interactions.stream()
+        .filter(i -> i.kind() == QueueInteractionKind.SEND).toList();
+    renderInteractions(bufferWriter, interactions, onlySendInteractions);
 
     out.write(template.replace("' diagram", buffer.toString()).getBytes());
   }
--- ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionTest.java
+++ ldj-spring-renderer-example/src/test/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionTest.java
@@ -11,38 +11,38 @@ class QueueInteractionTest {
 
   @Test
   void filterReadsReactingToThis() {
-    QueueInteraction post = new QueueInteraction("Kees", QueueInteractionKind.POST, "q1", "msg1",
+    QueueInteraction post = new QueueInteraction("Kees", QueueInteractionKind.SEND, "q1", "msg1",
         null);
-    QueueInteraction read = new QueueInteraction("Henk", QueueInteractionKind.READ, "q1", "msg1",
+    QueueInteraction read = new QueueInteraction("Henk", QueueInteractionKind.RECEIVE, "q1", "msg1",
         null);
-    QueueInteraction read2 = new QueueInteraction("Henks alterego", QueueInteractionKind.READ, "q1",
+    QueueInteraction read2 = new QueueInteraction("Henks alterego", QueueInteractionKind.RECEIVE, "q1",
         "msg1", null);
-    QueueInteraction readDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.READ, "q1",
+    QueueInteraction readDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.RECEIVE, "q1",
         "otherMsg", null);
-    QueueInteraction readDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.READ,
+    QueueInteraction readDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.RECEIVE,
         "otherQueue", "msg1", null);
     List<QueueInteraction> readInteractions = List.of(readDecoy1, read, readDecoy2, read2);
 
     List<QueueInteraction> expected = List.of(read, read2);
 
-    assertIterableEquals(expected, post.filterReadsReactingToThis(readInteractions));
+    assertIterableEquals(expected, post.filterReceivesReactingToThis(readInteractions));
   }
 
   @Test
   void is_read_on_post() {
-    QueueInteraction read = new QueueInteraction("henk", QueueInteractionKind.READ, "q1", "msg1",
+    QueueInteraction read = new QueueInteraction("henk", QueueInteractionKind.RECEIVE, "q1", "msg1",
         null);
-    QueueInteraction post = new QueueInteraction("kees", QueueInteractionKind.POST, "q1", "msg1",
+    QueueInteraction post = new QueueInteraction("kees", QueueInteractionKind.SEND, "q1", "msg1",
         null);
-    assertTrue(read.isReadOnPost(post));
-    assertFalse(post.isReadOnPost(read));
+    assertTrue(read.isReceiveOnSend(post));
+    assertFalse(post.isReceiveOnSend(read));
 
-    QueueInteraction postDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.POST, "q1",
+    QueueInteraction postDecoy1 = new QueueInteraction("Pieter", QueueInteractionKind.SEND, "q1",
         "otherMsg", null);
-    assertFalse(read.isReadOnPost(postDecoy1));
+    assertFalse(read.isReceiveOnSend(postDecoy1));
 
-    QueueInteraction postDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.POST,
+    QueueInteraction postDecoy2 = new QueueInteraction("Ingrid", QueueInteractionKind.SEND,
         "otherQueue", "msg1", null);
-    assertFalse(read.isReadOnPost(postDecoy2));
+    assertFalse(read.isReceiveOnSend(postDecoy2));
   }
 }
