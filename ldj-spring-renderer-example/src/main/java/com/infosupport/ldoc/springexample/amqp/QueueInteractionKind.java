c3b177ed26fa7e2df64dad27bd3dfd7eb36db02f
Guus Grievink <gs.grievink@gmail.com>
2023-12-16 14:43:27 +0100

    Added missing JavaDoc + addressed style issues.

--- ldj-spring-renderer-example/src/main/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionKind.java
+++ ldj-spring-renderer-example/src/main/java/com/infosupport/ldoc/springexample/amqp/QueueInteractionKind.java
@@ -1,5 +1,9 @@
 package com.infosupport.ldoc.springexample.amqp;
 
+/**
+ * Represents types of communication/interaction with a queue.
+ * Receive a message from a queue, or, send a message to a queue.
+ */
 public enum QueueInteractionKind {
   RECEIVE,
   SEND
