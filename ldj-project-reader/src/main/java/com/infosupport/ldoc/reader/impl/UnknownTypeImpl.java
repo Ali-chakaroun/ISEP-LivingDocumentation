b4f233146e28a0e3f5a91a1b1006bdeec43eb6fe
Wander Nauta <info@wandernauta.nl>
2023-12-21 21:55:57 +0100

    Make it possible to work with UnknownTypeImpl as recognized types

--- ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/impl/UnknownTypeImpl.java
+++ ldj-project-reader/src/main/java/com/infosupport/ldoc/reader/impl/UnknownTypeImpl.java
@@ -1,83 +1,17 @@
 package com.infosupport.ldoc.reader.impl;
 
-import com.infosupport.ldoc.reader.Attribute;
-import com.infosupport.ldoc.reader.Constructor;
-import com.infosupport.ldoc.reader.DocumentationComment;
-import com.infosupport.ldoc.reader.EnumMember;
-import com.infosupport.ldoc.reader.Event;
-import com.infosupport.ldoc.reader.Field;
-import com.infosupport.ldoc.reader.Method;
-import com.infosupport.ldoc.reader.Property;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.infosupport.ldoc.reader.Type;
 import com.infosupport.ldoc.reader.Visitor;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
-class UnknownTypeImpl implements Type {
+class UnknownTypeImpl extends TypeImpl implements Type {
+
+  UnknownTypeImpl(ProjectImpl project, JsonNode node) {
+    super(project, node);
+  }
 
   @Override
   public void accept(Visitor v) {
     /* Do nothing. */
-  }
-
-  @Override
-  public String fullName() {
-    return null;
-  }
-
-  @Override
-  public String name() {
-    return null;
-  }
-
-  @Override
-  public long modifiers() {
-    return 0;
-  }
-
-  @Override
-  public List<String> basetypes() {
-    return List.of();
-  }
-
-  @Override
-  public Stream<Field> fields() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<Constructor> constructors() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<Method> methods() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<Property> properties() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<Attribute> attributes() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<EnumMember> enumMembers() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Stream<Event> events() {
-    return Stream.empty();
-  }
-
-  @Override
-  public Optional<DocumentationComment> documentationComment() {
-    return Optional.empty();
   }
 }
