4f5607319f50d4e2f8152a9ec2524336452dc4db
Wander Nauta <info@wandernauta.nl>
2024-01-02 16:49:30 +0100

    Add KitchenSinkTest class

---
+++ ldj-project-reader/src/test/java/com/infosupport/ldoc/reader/KitchenSinkTest.java
@@ -0,0 +1,98 @@
+package com.infosupport.ldoc.reader;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.infosupport.ldoc.reader.impl.JacksonProjectFactory;
+import com.infosupport.ldoc.reader.visitor.BaseVisitor;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests with kitchensink.json, an output from the C# analyzer that covers the entire JSON Schema as
+ * much as possible.
+ */
+class KitchenSinkTest {
+
+  private final Project project;
+
+  KitchenSinkTest() throws IOException {
+    URL url = KitchenSinkTest.class.getResource("kitchensink.json");
+    project = new JacksonProjectFactory().project(url);
+  }
+
+  @Test
+  void baseVisitSucceeds() {
+    /* Given that we don't override anything in the BaseVisitor, it does not complain. */
+    assertDoesNotThrow(() -> project.accept(new BaseVisitor() {
+    }));
+  }
+
+  @Test
+  void typesByKind() {
+    /* Given that some classes, enums, interfaces and structs exist, we can find them. */
+    assertEquals(List.of("KitchenSink", "SomeClass"), project.classes().map(Named::name).toList());
+    assertEquals(List.of("ExampleEnum"), project.enums().map(Named::name).toList());
+    assertEquals(List.of("IExampleInterface"), project.interfaces().map(Named::name).toList());
+    assertEquals(List.of("ExampleStruct"), project.structs().map(Named::name).toList());
+  }
+
+  @Test
+  void documentationComment() {
+    /* Exactly one documentation comment exists, and when read, it matches our expectations. */
+    List<DocumentationComment> seen = new ArrayList<>();
+
+    project.accept(new BaseVisitor() {
+      @Override
+      public void visitDocumentationComment(DocumentationComment doc) {
+        assertEquals("Summary goes here.", doc.summary());
+        assertEquals("Remarks go here.", doc.remarks());
+        assertEquals("Returned value goes here.", doc.returns());
+        assertEquals(Map.of("args", "Description for args goes here."), doc.params());
+        assertEquals(Set.of("EverythingApp.ExampleEnum"), doc.seeAlsos().keySet());
+        assertEquals(
+            Map.of("System.IO.IOException", "IOException description goes here."),
+            doc.exceptions());
+
+        /* Missing tags are null. */
+        assertNull(doc.example());
+        assertNull(doc.value());
+        assertNull(doc.typeParams());
+        assertNull(doc.permissions());
+
+        seen.add(doc);
+      }
+    });
+
+    assertEquals(1, seen.size());
+  }
+
+  @Test
+  void attribute() {
+    /* Exactly one attribute exists in the entire project and it has exactly the shape we expect. */
+    List<Attribute> seen = new ArrayList<>();
+
+    project.accept(new BaseVisitor() {
+      @Override
+      public void visitAttribute(Attribute attrib) {
+        assertEquals("Obsolete", attrib.name());
+        assertEquals("System.ObsoleteAttribute", attrib.type());
+
+        assertEquals(1, attrib.arguments().peek(arg -> {
+          assertEquals("Example attribute", arg.value());
+          assertEquals("string", arg.type());
+        }).count());
+
+        seen.add(attrib);
+      }
+    });
+
+    assertEquals(1, seen.size());
+  }
+}
